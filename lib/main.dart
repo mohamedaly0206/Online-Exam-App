2e89ba4a7d375bf3cffdb777b1c52dfcf47490c6
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-03-08 16:38:35 +0200

    add auth feature folder & and remove debug Mode Banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class OnlineExamApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.appTheme,
       home: Scaffold(),
     );
