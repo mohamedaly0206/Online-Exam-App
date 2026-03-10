09635cba3822a859340d7b01a2e189cd1db0951a
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-03-10 02:26:09 +0200

    feat(forget-password): create api client file & add api param for this feature

--- lib/core/values/api_param.dart
+++ lib/core/values/api_param.dart
@@ -1,8 +1,9 @@
 import 'package:online_exam_app/core/values/constants.dart';
 
 class ApiParam {
-  static const String getSubjectExams =
-      '$baseUrl/exams?subject=';
-  static const String getAllExamQuestions =
-      '$baseUrl/questions?exam=';
+  static const String getSubjectExams = '$baseUrl/exams?subject=';
+  static const String getAllExamQuestions = '$baseUrl/questions?exam=';
+  static const String email = 'email';
+  static const String resetCode = 'resetCode';
+  static const String newPassword = 'newPassword';
 }
---
+++ lib/features/auth/forget_password/api/api_client/forget_password_api_client.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:online_exam_app/core/values/api_param.dart';
+import 'package:online_exam_app/features/auth/forget_password/data/model/forget_password_dto.dart';
+import 'package:online_exam_app/features/auth/forget_password/data/model/reset_password_dto.dart';
+import 'package:online_exam_app/features/auth/forget_password/data/model/verify_reset_code_dto.dart';
+import '/core/values/api_endpoints.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'forget_password_api_client.g.dart';
+
+@lazySingleton
+@RestApi()
+abstract class ForgetPasswordApiClient {
+  @factoryMethod
+  factory ForgetPasswordApiClient(Dio dio) = _ForgetPasswordApiClient;
+
+  @POST(ApiEndpoints.forgetPassword)
+  Future<ForgetPasswordDTO> forgetPassword(
+    @Query(ApiParam.email) String? email,
+  );
+
+  // there is an error here it don't know which account that have this resetCode(OTP)
+  @POST(ApiEndpoints.verifyResetPassword)
+  Future<VerifyResetCodeDTO> verifyResetCode(
+    @Query(ApiParam.resetCode) String? resetCode,
+  );
+
+  @PUT(ApiEndpoints.resetPassword)
+  Future<ResetPasswordDTO> resetPassword(
+    @Query(ApiParam.email) String? email,
+    @Query(ApiParam.newPassword) String? newPassword,
+  );
+}
