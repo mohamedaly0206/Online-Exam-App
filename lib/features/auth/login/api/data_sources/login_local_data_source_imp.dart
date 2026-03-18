8bc6251c7e09bda0670200a9bb3364a1a5a48f55
Khalid Adel <khalidadel209@gmail.com>
2026-03-18 07:16:00 +0200

    feat(login): refactor code and finish login repo imp

--- lib/features/auth/login/api/data_sources/login_local_data_source_imp.dart
+++ lib/features/auth/login/api/data_sources/login_local_data_source_imp.dart
@@ -1,11 +1,18 @@
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/config/security_storage/security_storage_module.dart';
 import 'package:online_exam_app/features/auth/login/data/data_sources/login_local_data_source_contract.dart';
+import '../../../../../core/errors/exceptions.dart';
 
 @Injectable(as: LoginLocalDataSourceContract)
 class LoginLocalDataSourceImp implements LoginLocalDataSourceContract {
   @override
   Future<void> saveToken(String token) async {
-    await SecurityStorageModule.setSecuredString('token', token);
+    try {
+      await SecurityStorageModule.setSecuredString('token', token);
+    } catch (e) {
+      throw const CacheException(
+        errorMessage: 'Failed to save data locally, please try again later.',
+      );
+    }
   }
 }
