e81d1bb5b776228d07313e6022e108702f8c529f
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-03-19 22:27:23 +0200

    fix(develop): fix app failures and app strings

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,17 +1,25 @@
 import 'package:dio/dio.dart';
+import 'package:online_exam_app/core/values/app_strings.dart';
 
-abstract class Failure  {
+abstract class Failure {
   final String errorMessage;
   const Failure(this.errorMessage);
 }
 
-class ServerFailure extends Failure{
+class ServerFailure extends Failure {
   ServerFailure(super.errorMessage);
 
-  factory ServerFailure.fromDioException(DioException exception){
-    switch(exception.type){
-      case DioExceptionType.connectionTimeout:
+  static ServerFailure failureHandler(Exception e) {
+    if (e is DioException) {
+      return ServerFailure.fromDioException(e);
+    } else {
+      return ServerFailure(AppStrings.errorMessage);
+    }
+  }
 
+  factory ServerFailure.fromDioException(DioException exception) {
+    switch (exception.type) {
+      case DioExceptionType.connectionTimeout:
         return ServerFailure('Connection timeout with API server');
       case DioExceptionType.sendTimeout:
         return ServerFailure('Send timeout with API server');
@@ -20,7 +28,10 @@ class ServerFailure extends Failure{
       case DioExceptionType.badCertificate:
         return ServerFailure('Bad certificate with API server');
       case DioExceptionType.badResponse: //**********
-        return ServerFailure.fromResponse(exception.response!.statusCode!, exception.response!.data);
+        return ServerFailure.fromResponse(
+          exception.response!.statusCode!,
+          exception.response!.data,
+        );
       case DioExceptionType.cancel:
         return ServerFailure('Request to API server was cancelled');
       case DioExceptionType.connectionError:
@@ -29,42 +40,24 @@ class ServerFailure extends Failure{
         return ServerFailure('No Internet Connection');
     }
   }
-  factory ServerFailure.fromResponse(int statusCode,dynamic response){
-    if(statusCode == 400 || statusCode == 401 || statusCode == 403){
-      // final editError = response['user'] != null ? response['user'] : null;
-      // final loginError = response['detail'] != null ? response['detail'] : null;
-      // final emailError = response['email'] != null ? response['email'][0] : null;
-      // final changePasswordEmailError = response['detail'] != null ? response['detail'] : null;
-      //
-      // final phoneError = response['phone_number'] != null ? response['phone_number'][0] : null;
-      // String message;
-      // if (emailError != null && phoneError != null) {
-      //   print('email & pass  repeated');
-      //   message = 'Both the email and phone number are already registered.';
-      // } else if (emailError != null) {
-      //   message = emailError;
-      // } else if (phoneError != null) {
-      //   message = phoneError;
-      // }// login handle if wrong email or pass
-      // else if (loginError != null) {
-      //   message = loginError;
-      // } else if(editError != null){
-      //   message = editError['email'];
-      // }else if (changePasswordEmailError != null) {
-      //   message = changePasswordEmailError;
-      // }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      final String errorMessageRes =
+          response['message'] ?? 'Opps, there was an error';
+      if (errorMessageRes.contains('required pattern')) {
+        return ServerFailure('Invalid email or password');
+      }
 
-      return ServerFailure('error message status code 400');
-    }
-    else if(statusCode == 404){
+      return ServerFailure(errorMessageRes);
+    } else if (statusCode == 404) {
       return ServerFailure('Opps there was an error, please try again');
-    }
-    else if (statusCode == 500){
+    } else if (statusCode == 409) {
+      final message = response['message'].toString();
+      return ServerFailure(message);
+    } else if (statusCode == 500) {
       return ServerFailure('Internal server error, please try again later');
-    }
-    else{
-      return ServerFailure('Opps there was an error, please try again');
+    } else {
+      return ServerFailure('Opps there was an error, please try again66');
     }
   }
 }
-
