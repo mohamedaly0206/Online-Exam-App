df980f56ce6aa3f58b3d721aa7b62af646f72b42
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-03-19 22:44:31 +0200

    fix(forget-password): fix error handler

--- lib/features/auth/forget_password/api/data_source/forget_password_remote_data_source_impl.dart
+++ lib/features/auth/forget_password/api/data_source/forget_password_remote_data_source_impl.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/config/base_response/base_response.dart';
 import 'package:online_exam_app/core/errors/failures.dart';
@@ -25,13 +24,9 @@ class ForgetPasswordRemoteDataSourceImpl
         ApiParam.email: email,
       });
       return SuccessBaseResponse<ForgetPasswordDTO>(data: response);
-    } on DioException catch (e) {
-      return ErrorBaseResponse<ForgetPasswordDTO>(
-        errorMessage: ServerFailure.fromDioException(e).errorMessage,
-      );
     } catch (e) {
       return ErrorBaseResponse<ForgetPasswordDTO>(
-        errorMessage: 'unexpected error',
+        errorMessage:ServerFailure.failureHandler(e).errorMessage,
       );
     }
   }
@@ -47,12 +42,8 @@ class ForgetPasswordRemoteDataSourceImpl
         ApiParam.newPassword: newPassword,
       });
       return SuccessBaseResponse<ResetPasswordDTO>(data: response);
-    } on DioException catch (e) {
-      return ErrorBaseResponse(
-        errorMessage: ServerFailure.fromDioException(e).errorMessage,
-      );
-    } catch (e) {
-      return ErrorBaseResponse(errorMessage: 'unexpected error');
+    }  catch (e) {
+      return ErrorBaseResponse( errorMessage:ServerFailure.failureHandler(e).errorMessage,);
     }
   }
 
@@ -65,12 +56,8 @@ class ForgetPasswordRemoteDataSourceImpl
         ApiParam.resetCode: resetCode,
       });
       return SuccessBaseResponse<VerifyResetCodeDTO>(data: response);
-    } on DioException catch (e) {
-      return ErrorBaseResponse<VerifyResetCodeDTO>(
-        errorMessage: ServerFailure.fromDioException(e).errorMessage,
-      );
-    } catch (e) {
-      return ErrorBaseResponse<VerifyResetCodeDTO>(errorMessage: 'unexpected error');
+    }  catch (e) {
+      return ErrorBaseResponse<VerifyResetCodeDTO>(errorMessage:ServerFailure.failureHandler(e).errorMessage,);
     }
   }
 }
