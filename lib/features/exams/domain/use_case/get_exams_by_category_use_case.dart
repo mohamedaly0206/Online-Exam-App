b658923a7574ef1245c4e43395f82a51d0d3def9
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-03-27 20:38:09 +0200

    feat(exams): create feature use cases

---
+++ lib/features/exams/domain/use_case/get_exams_by_category_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../../config/base_response/base_response.dart';
+import '../model/exams_model.dart';
+import '../repo/exams_repo_contract.dart';
+
+@injectable
+class GetExamsByCategoryUseCase {
+  GetExamsByCategoryUseCase({required this.examsRepoContract});
+  final ExamsRepoContract examsRepoContract;
+
+  Future<BaseResponse<List<ExamModel>>> call({
+    required String token,
+    required String? subjectId,
+  }) => examsRepoContract.getExams(token: token, subjectId: subjectId);
+}
