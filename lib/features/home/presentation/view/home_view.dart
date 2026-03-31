774f2211e50abffd2102fede39cdcad4b34d2908
Khalid Adel <khalidadel209@gmail.com>
2026-03-31 04:04:51 +0200

    feat(home): refactor theme file and bottom navigation

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -5,6 +5,7 @@ import 'package:online_exam_app/features/home/presentation/view_model/cubit/home
 import 'package:online_exam_app/features/home/presentation/view_model/state/home_states.dart';
 import 'package:online_exam_app/features/home/presentation/widgets/home_view_body.dart';
 
+import '../../../../core/values/app_strings.dart';
 import '../../../../core/values/assets.gen.dart';
 import '../view_model/intent/home_intent.dart';
 
@@ -13,8 +14,8 @@ class HomeView extends StatelessWidget {
 
   static const List<Widget> _tabs = [
     HomeViewBody(),
-    Center(child: Text('Results')),
-    Center(child: Text('Profile')),
+    Center(child: Text(AppStrings.result)),
+    Center(child: Text(AppStrings.profile)),
   ];
 
   @override
@@ -26,70 +27,54 @@ class HomeView extends StatelessWidget {
         return Scaffold(
           // to save the state of each tab when switching between them
           body: IndexedStack(index: state.currentIndex, children: _tabs),
-          bottomNavigationBar: NavigationBarTheme(
-            data: NavigationBarThemeData(
-              indicatorColor: Theme.of(
+          bottomNavigationBar: NavigationBar(
+            selectedIndex: state.currentIndex,
+            onDestinationSelected: (index) {
+              BlocProvider.of<HomeCubit>(
                 context,
-              ).colorScheme.primary.withOpacity(0.12),
-              labelTextStyle: WidgetStateProperty.all(
-                Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
+              ).doIntent(ChangeTapIntent(index));
+            },
+            indicatorColor: Theme.of(
+              context,
+            ).colorScheme.primary.withOpacity(0.12),
+            destinations: [
+              NavigationDestination(
+                icon: SvgPicture.asset(
+                  Assets.icons.homeIcon,
+                  height: 30,
+                  width: 30,
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).primaryColor,
+                    BlendMode.srcIn,
+                  ),
                 ),
+                label: AppStrings.explore,
               ),
-              labelPadding: const EdgeInsets.only(top: 5),
-            ),
-            child: NavigationBar(
-              height: 60,
-              selectedIndex: state.currentIndex,
-
-              onDestinationSelected: (index) {
-                BlocProvider.of<HomeCubit>(
-                  context,
-                ).doIntent(ChangeTapIntent(index));
-              },
-              indicatorColor: Theme.of(
-                context,
-              ).colorScheme.primary.withOpacity(0.12),
-              destinations: [
-                NavigationDestination(
-                  icon: SvgPicture.asset(
-                    Assets.icons.homeIcon,
-                    height: 30,
-                    width: 30,
-                    colorFilter: ColorFilter.mode(
-                      Theme.of(context).primaryColor,
-                      BlendMode.srcIn,
-                    ),
+              NavigationDestination(
+                icon: SvgPicture.asset(
+                  Assets.icons.resultIcon,
+                  height: 30,
+                  width: 30,
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).primaryColor,
+                    BlendMode.srcIn,
                   ),
-                  label: 'Explore',
                 ),
-                NavigationDestination(
-                  icon: SvgPicture.asset(
-                    Assets.icons.resultIcon,
-                    height: 30,
-                    width: 30,
-                    colorFilter: ColorFilter.mode(
-                      Theme.of(context).primaryColor,
-                      BlendMode.srcIn,
-                    ),
+                label: AppStrings.result,
+              ),
+              NavigationDestination(
+                icon: SvgPicture.asset(
+                  Assets.icons.personIcon,
+                  height: 30,
+                  width: 30,
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).primaryColor,
+                    BlendMode.srcIn,
                   ),
-                  label: 'Results',
                 ),
-                NavigationDestination(
-                  icon: SvgPicture.asset(
-                    Assets.icons.personIcon,
-                    height: 30,
-                    width: 30,
-                    colorFilter: ColorFilter.mode(
-                      Theme.of(context).primaryColor,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                  label: 'Profile',
-                ),
-              ],
-            ),
+                label: AppStrings.profile,
+              ),
+            ],
           ),
         );
       },
--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_exam_app/core/theme/app_colors.dart';
+import 'package:online_exam_app/core/values/app_strings.dart';
+import 'package:online_exam_app/core/values/assets.gen.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('<<<<<<<<<you r now in home>>>>>>>>>>'));
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              AppStrings.survey,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: AppStrings.search,
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10.0,
+                    bottom: 10,
+                    left: 16,
+                    right: 0,
+                  ),
+                  child: SvgPicture.asset(
+                    Assets.icons.searchIcon,
+                    colorFilter: ColorFilter.mode(
+                      AppColors.placeHolderColor,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  //borderSide: BorderSide(color: Theme.of(context)),
+                ),
+                hintStyle: Theme.of(context).textTheme.displayLarge!.copyWith(
+                  color: AppColors.placeHolderColor,
+                ),
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
