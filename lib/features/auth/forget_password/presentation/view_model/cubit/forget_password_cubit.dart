c4937c0ca40b18bd23543e6d9aca60f5557ea9d3
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-04-16 10:20:59 +0200

    fix(forget-password): replace hardcoded email string with ApiParam constant for better maintainability

--- lib/features/auth/forget_password/presentation/view_model/cubit/forget_password_cubit.dart
+++ lib/features/auth/forget_password/presentation/view_model/cubit/forget_password_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/config/base_response/base_response.dart';
 import 'package:online_exam_app/config/security_storage/security_storage_module.dart';
+import 'package:online_exam_app/core/values/api_param.dart';
 import '../../../domain/entity/forget_password_entity.dart';
 import '../../../domain/entity/reset_password_entity.dart';
 import '../../../domain/entity/verify_reset_code_entity.dart';
@@ -42,11 +43,11 @@ class ForgetPasswordCubit extends Cubit<ForgetPasswordState> {
   }
 
   void _saveEmailLocally(String email) {
-    SecurityStorageModule.setSecuredString('email', email);
+    SecurityStorageModule.setSecuredString(ApiParam.email, email);
   }
 
   Future<String> _getEmailFromLocal() async {
-    return await SecurityStorageModule.getSecuredString('email');
+    return await SecurityStorageModule.getSecuredString(ApiParam.email);
   }
 
   Future<void> _sendResetEmail(String email) async {
