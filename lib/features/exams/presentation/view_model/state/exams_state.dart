96360e706a78d66eb55b4295c9ddb6c5cab47de2
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-04-21 03:08:21 +0200

    refactor(exams): remove unused injectable annotation from ExamsState

--- lib/features/exams/presentation/view_model/state/exams_state.dart
+++ lib/features/exams/presentation/view_model/state/exams_state.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
-import 'package:injectable/injectable.dart';
-
 import '../../../../../config/base_state/base_state.dart';
 import '../../../domain/entity/exams_entity.dart';
 
-@injectable
 class ExamsState extends Equatable {
   const ExamsState({this.examsState = const BaseState<List<ExamEntity>>()});
   final BaseState<List<ExamEntity>> examsState;
