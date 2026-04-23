b4108044692e0cae12ecf435ef907d6ae75456c4
Khalid Adel <khalidadel209@gmail.com>
2026-04-23 09:43:28 +0200

    feat(edit-profile): provide cubit & refactor edit profile view

--- lib/features/edit_profile/presentation/view/edit_profile_view.dart
+++ lib/features/edit_profile/presentation/view/edit_profile_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_exam_app/config/di/di.dart';
 import 'package:online_exam_app/core/values/app_strings.dart';
 import 'package:online_exam_app/core/widgets/custom_app_bar.dart';
+import 'package:online_exam_app/features/edit_profile/presentation/view_model/cubit/edit_profile_cubit.dart';
 import 'package:online_exam_app/features/edit_profile/presentation/widgets/edit_profile_view_body.dart';
+
+import '../view_model/intent/edit_profile_intent.dart';
 
 class EditProfileView extends StatelessWidget {
   const EditProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(hasBackButton: false, title: AppStrings.profile),
-      body: const EditProfileViewBody(),
+    return BlocProvider(
+      create: (context) =>
+          getIt<EditProfileCubit>()..processIntent(GetUserInfoIntent()),
+      child: Scaffold(
+        appBar: CustomAppBar(hasBackButton: false, title: AppStrings.profile),
+        body: const EditProfileViewBody(),
+      ),
     );
   }
 }
--- lib/features/edit_profile/presentation/widgets/edit_profile_view_body.dart
+++ lib/features/edit_profile/presentation/widgets/edit_profile_view_body.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_exam_app/core/values/app_strings.dart';
 
 import '../../../../core/utilities/app_validators.dart';
+import '../view_model/cubit/edit_profile_cubit.dart';
+import '../view_model/intent/edit_profile_intent.dart';
+import '../view_model/state/edit_profile_state.dart';
 
 class EditProfileViewBody extends StatefulWidget {
   const EditProfileViewBody({super.key});
@@ -23,31 +27,34 @@ class _EditProfileViewBodyState extends State<EditProfileViewBody> {
   @override
   void initState() {
     super.initState();
+    _passwordController.text = '********';
     _userNameController.addListener(_checkInputs);
     _firstNameController.addListener(_checkInputs);
     _lastNameController.addListener(_checkInputs);
     _emailController.addListener(_checkInputs);
-    _passwordController.addListener(_checkInputs);
     _phoneController.addListener(_checkInputs);
   }
 
   void _checkInputs() {
-    bool allFieldsAreEmpty =
-        _userNameController.text.isEmpty &&
-        _firstNameController.text.isEmpty &&
-        _lastNameController.text.isEmpty &&
-        _emailController.text.isEmpty &&
-        _passwordController.text.isEmpty &&
-        _phoneController.text.isEmpty;
-    if (allFieldsAreEmpty == true) {
-      setState(() {
-        _isButtonEnabled = false;
-      });
-    } else {
-      setState(() {
-        _isButtonEnabled = true;
-      });
+    final state = context.read<EditProfileCubit>().state;
+    // data in cubit
+    final user = state.editProfileState.data;
+
+    if (user == null) {
+      setState(() => _isButtonEnabled = false);
+      return;
     }
+
+    bool isChanged =
+        _userNameController.text != (user.userName) ||
+        _firstNameController.text != (user.firstName) ||
+        _lastNameController.text != (user.lastName) ||
+        _emailController.text != (user.email) ||
+        _phoneController.text != (user.phone);
+
+    setState(() {
+      _isButtonEnabled = isChanged;
+    });
   }
 
   @override
@@ -63,152 +70,177 @@ class _EditProfileViewBodyState extends State<EditProfileViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  Container(
-                    width: 105,
-                    height: 105,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          'https://www.mnp.ca/-/media/foundation/integrations/personnel/2020/12/16/13/57/personnel-image-4483.jpg?h=800&iar=0&w=600&hash=833D605FDB6AC3C2D2915F6BF8B4ADA4',
-                        ),
+    return BlocListener<EditProfileCubit, EditProfileState>(
+      listenWhen: (previous, current) =>
+          previous.editProfileState.data == null &&
+          current.editProfileState.data != null,
+      listener: (context, state) {
+        final user = state.editProfileState.data;
+        _userNameController.text = user?.userName ?? '';
+        _firstNameController.text = user?.firstName ?? '';
+        _lastNameController.text = user?.lastName ?? '';
+        _emailController.text = user?.email ?? '';
+        _phoneController.text = user?.phone ?? '';
+      },
+      child: BlocBuilder<EditProfileCubit, EditProfileState>(
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          Container(
+                            width: 105,
+                            height: 105,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                  'https://www.mnp.ca/-/media/foundation/integrations/personnel/2020/12/16/13/57/personnel-image-4483.jpg?h=800&iar=0&w=600&hash=833D605FDB6AC3C2D2915F6BF8B4ADA4',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 33,
+                            width: 33,
+                            decoration: BoxDecoration(
+                              color: Theme.of(
+                                context,
+                              ).colorScheme.onSecondaryFixed,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              size: 20,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  Container(
-                    height: 33,
-                    width: 33,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.onSecondaryFixed,
-                      borderRadius: BorderRadius.circular(8),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _userNameController,
+                      decoration: const InputDecoration(
+                        label: Text(AppStrings.userName),
+                      ),
                     ),
-                    child: Icon(
-                      Icons.camera_alt_outlined,
-                      size: 20,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _userNameController,
-              decoration: const InputDecoration(
-                label: Text(AppStrings.userName),
-                hintText: 'Enter your user name',
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: _firstNameController,
-                    decoration: const InputDecoration(
-                      label: Text(AppStrings.firstName),
-                      hintText: 'Enter first name',
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: TextFormField(
-                    controller: _lastNameController,
-                    decoration: const InputDecoration(
-                      label: Text(AppStrings.lastName),
-                      hintText: 'Enter last name',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                label: Text(AppStrings.email),
-                hintText: 'Enter your email',
-              ),
-              validator: (value) => (value == null || value.isEmpty)
-                  ? null
-                  : AppValidators.validateEmail(value),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _passwordController,
-              validator: (value) => (value == null || value.isEmpty)
-                  ? null
-                  : AppValidators.validatePassword(value),
-              decoration: InputDecoration(
-                suffixIcon: Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: Theme.of(context).textTheme.displayLarge
-                          ?.copyWith(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontWeight: FontWeight.w600,
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: _firstNameController,
+                            decoration: const InputDecoration(
+                              label: Text(AppStrings.firstName),
+                            ),
                           ),
-                    ),
-                    onPressed: () {},
-                    child: Text(AppStrings.change),
-                  ),
-                ),
-                label: Text(AppStrings.password),
-                hintText: 'Enter your password',
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _phoneController,
-              decoration: const InputDecoration(
-                label: Text(AppStrings.phone),
-                hintText: 'Enter your phone number',
-              ),
-              validator: (value) => (value == null || value.isEmpty)
-                  ? null
-                  : AppValidators.validatePhoneNumber(value),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 60),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(side: BorderSide.none),
-              onPressed: _isButtonEnabled
-                  ? () {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Data updated Successfully'),
-                            backgroundColor: Colors.green,
-                            duration: Duration(seconds: 2),
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: TextFormField(
+                            controller: _lastNameController,
+                            decoration: const InputDecoration(
+                              label: Text(AppStrings.lastName),
+                            ),
                           ),
-                        );
-                      }
-                    }
-                  : null,
-              child: Text(
-                AppStrings.updateButton,
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.onSecondary,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        label: Text(AppStrings.email),
+                      ),
+                      validator: (value) => (value == null || value.isEmpty)
+                          ? null
+                          : AppValidators.validateEmail(value),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.only(right: 12.0),
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: Theme.of(context).textTheme.bodyLarge
+                                  ?.copyWith(
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.primary,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            onPressed: () {},
+                            child: const Text(AppStrings.change),
+                          ),
+                        ),
+                        label: const Text(AppStrings.password),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _phoneController,
+                      decoration: const InputDecoration(
+                        label: Text(AppStrings.phone),
+                      ),
+                      validator: (value) => (value == null || value.isEmpty)
+                          ? null
+                          : AppValidators.validatePhoneNumber(value),
+                      keyboardType: TextInputType.number,
+                    ),
+                    const SizedBox(height: 60),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(side: BorderSide.none),
+                      onPressed:
+                          (_isButtonEnabled &&
+                              !state.editProfileState.isLoading)
+                          ? () {
+                              if (_formKey.currentState!.validate()) {
+                                context.read<EditProfileCubit>().processIntent(
+                                  SubmitEditProfileIntent(
+                                    username: _userNameController.text,
+                                    firstName: _firstNameController.text,
+                                    lastName: _lastNameController.text,
+                                    email: _emailController.text,
+                                    phone: _phoneController.text,
+                                  ),
+                                );
+                              }
+                            }
+                          : null,
+                      child: state.editProfileState.isLoading
+                          ? CircularProgressIndicator()
+                          : Text(
+                              AppStrings.updateButton,
+                              style: Theme.of(context).textTheme.titleMedium
+                                  ?.copyWith(
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.onSecondary,
+                                  ),
+                            ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
