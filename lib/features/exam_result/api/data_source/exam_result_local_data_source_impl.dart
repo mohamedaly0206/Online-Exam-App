85838f8e0c5d21f78247dc2fc827e76dae2e11a8
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-05-02 00:52:38 +0300

    feat(exam_result): add logging for caching and retrieving exam results

--- lib/features/exam_result/api/data_source/exam_result_local_data_source_impl.dart
+++ lib/features/exam_result/api/data_source/exam_result_local_data_source_impl.dart
@@ -1,4 +1,5 @@
-import 'package:hive/hive.dart';
+import 'dart:developer';
+import 'package:hive_ce/hive_ce.dart';
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/config/base_response/base_response.dart';
 import 'package:online_exam_app/features/exam_result/data/data_source/exam_result_local_data_source_contract.dart';
@@ -15,10 +16,14 @@ class ExamResultLocalDataSourceImpl
     String subjectName,
   ) async {
     try {
+      print('!! 1');
       final box = await Hive.openBox('exam_results');
+      print('!! 2');
       await box.add(ExamResultModelDTO.fromDomain(examResult, subjectName));
+      print('examResult cached successfully: $examResult');
       return SuccessBaseResponse<bool>(data: true);
     } catch (e) {
+      print('Error caching exam result: $e');
       return ErrorBaseResponse<bool>(errorMessage: e.toString());
     }
   }
@@ -27,20 +32,31 @@ class ExamResultLocalDataSourceImpl
   Future<BaseResponse<List<SubjectResultModelDTO>>>
   getExamsResultsHistory() async {
     try {
+      log(
+        '>>>>>>>>>>>>>>>>>getting exam results history from local data source',
+      );
+      log('>> 1');
       final box = await Hive.openBox('exam_results');
+      log('>> 2');
       if (box.isEmpty) {
+        log('>> 3');
         return SuccessBaseResponse<List<SubjectResultModelDTO>>(data: []);
       }
+      log('>> 4');
       final allResults = box.values.cast<ExamResultModelDTO>().toList();
+      log('>> 5');
       final Map<String, List<ExamResultModelDTO>> groupedResults = {};
       for (final result in allResults) {
+        log('>> 6');
         if (groupedResults.containsKey(result.subjectName)) {
+          log('>> 7');
           groupedResults[result.subjectName]!.add(result);
         } else {
+          log('>> 8');
           groupedResults[result.subjectName] = [result];
         }
       }
-
+      log('>> 9');
       return SuccessBaseResponse<List<SubjectResultModelDTO>>(
         data: groupedResults.entries
             .map(
@@ -52,6 +68,7 @@ class ExamResultLocalDataSourceImpl
             .toList(),
       );
     } catch (e) {
+      log('>> 10');
       return ErrorBaseResponse<List<SubjectResultModelDTO>>(
         errorMessage: e.toString(),
       );
--- lib/features/exam_result/data/repo/exam_result_repo_impl.dart
+++ lib/features/exam_result/data/repo/exam_result_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/config/base_response/base_response.dart';
 import 'package:online_exam_app/features/exam_result/data/data_source/exam_result_local_data_source_contract.dart';
@@ -15,20 +17,38 @@ class ExamResultRepoImpl implements ExamResultRepoContract {
   Future<BaseResponse<bool>> cacheExamResult(
     ExamResultEntity examResult,
     String subjectName,
-  ) async{
-    return await localDataSource.cacheExamResult(examResult, subjectName);
+  ) async {
+    final response = await localDataSource.cacheExamResult(
+      examResult,
+      subjectName,
+    );
+    switch (response) {
+      case SuccessBaseResponse<bool>():
+        log('exam result cached successfully');
+        return SuccessBaseResponse<bool>(data: true);
+      case ErrorBaseResponse<bool>():
+        log('error caching exam result: ${response.errorMessage}');
+        return ErrorBaseResponse<bool>(errorMessage: response.errorMessage);
+    }
   }
 
   @override
-  Future<BaseResponse<List<SubjectResultEntity>>> getExamsResultsHistory() async{
-    final response= await localDataSource.getExamsResultsHistory();
+  Future<BaseResponse<List<SubjectResultEntity>>>
+  getExamsResultsHistory() async {
+    final response = await localDataSource.getExamsResultsHistory();
 
-    switch(response){
+    switch (response) {
       case SuccessBaseResponse<List<SubjectResultModelDTO>>():
+        log(
+          'got data from local data source: ${response.data.length} subjects',
+        );
         return SuccessBaseResponse<List<SubjectResultEntity>>(
           data: response.data.map((e) => e.toDomain()).toList(),
         );
       case ErrorBaseResponse<List<SubjectResultModelDTO>>():
+        log(
+          'error getting data from local data source: ${response.errorMessage}',
+        );
         return ErrorBaseResponse<List<SubjectResultEntity>>(
           errorMessage: response.errorMessage,
         );
